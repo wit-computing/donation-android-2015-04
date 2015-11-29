688c52992900c889eae3c8bfc0de66d826521e90
John Fitzgerald <usplitu@hotmail.com>
2015-11-29 09:30:49 +0000

    refactored Settings - removed SettingsFragment

--- app/src/main/java/app/donation/settings/SettingsActivity.java
+++ app/src/main/java/app/donation/settings/SettingsActivity.java
@@ -1,25 +1,27 @@
 package app.donation.settings;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 
+import app.android.helpers.LogHelpers;
 import app.donation.R;
 
 
-public class SettingsActivity extends Activity
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener
 {
+
+  private SharedPreferences prefs;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-    if (savedInstanceState == null)
-    {
-      SettingsFragment fragment = new SettingsFragment();
-      getFragmentManager().beginTransaction().add(android.R.id.content, fragment, fragment.getClass().getSimpleName())
-          .commit();
-    }
+    setupActionBar();
   }
 
   @Override
@@ -27,5 +29,36 @@ public class SettingsActivity extends Activity
   {
     getMenuInflater().inflate(R.menu.status, menu);
     return true;
+  }
+
+  private void setupActionBar()
+  {
+    ActionBar actionBar = getActionBar();
+    if (actionBar != null)
+    {
+      // Show the Up button in the action bar.
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  @Override
+  public void onStart()
+  {
+    super.onStart();
+    prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    prefs.registerOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onStop()
+  {
+    super.onStop();
+    prefs.unregisterOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+  {
+    LogHelpers.info(this, "change to preference whose key is " + key);
   }
 }
